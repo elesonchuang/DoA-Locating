86108766f3571347bfff07b8856d9ddcd057fe15
Davis Chou <lxdfs58@gmail.com>
2021-08-03 16:40:53 +0800

    Functions Update

--- altered/GetSignal.py
+++ altered/GetSignal.py
@@ -1,17 +1,21 @@
 from subprocess import Popen, PIPE # Used to run native OS commads in python wrapped subproccess
 def get_Signal(interface, freq, name):
-    #scan_command = ['sudo','iw','dev',interface,'scan','ssid',name]
-    scan_command = ['sudo','iw','dev',interface,'scan','freq',freq]
+    scan_command = ['sudo','iw','dev',interface,'scan','freq',freq,'ssid',name]
     scan_process = Popen(scan_command, stdout=PIPE, stderr=PIPE)
     (raw_output, raw_error) = scan_process.communicate()
     scan_process.wait()
-    raw_output = str(raw_output)
-    name = name #+ '\n'
-    for i in range(len(raw_output.split('signal:'))-1):
-        ssid = raw_output.split('SSID:')[i+1]
-        ssid = ssid.split(' ')[1]
-        if ssid.startswith(name):
-            signal = raw_output.split('signal:')[i+1]
-            signal = float(signal.split(' ')[1])
-            return signal
-#print(raw_output, raw_error)
+    raw_output = raw_output.decode() 
+    data = raw_output.split('\n')
+    info ={} #create return dictionary
+    #print('raw_output', type(raw_output), raw_output) 
+    for i in range(len(raw_output.split('\n'))):
+        if data[i].endswith(name):
+            for m in range(-5,5):
+                if data[i-m].endswith('channel', 0,-2):
+                    info['channel'] = (data[i-m].split())[-1]
+                elif data[i-m].endswith('ago'):
+                    info['interval'] = (data[i-m].split())[-3]    
+                elif data[i-m].endswith('dBm'):
+                    info['signal'] = (data[i-m].split())[1]
+            return info
+    return len(raw_output.split('\n'))
