5a2009cb68d54a8d0915f8119ccd346e34388e6c
fafachen61 <71396110+fafachen61@users.noreply.github.com>
2021-08-20 19:22:02 +0800

    new model

--- altered/RPI_servomotor.py
+++ altered/RPI_servomotor.py
@@ -9,12 +9,13 @@ import paho.mqtt.client as receive #import library
 # Open csv file
 f = open('ground_truth_new.csv', 'w')
 writer = csv.writer(f)
-total_portion = 10
+total_portion = 90
 each_portion = 180/total_portion
 sum_signal = 10
 diff_signal = 20
 angle = 0
-duty = angle*18 +2
+record_angle = -90
+duty = angle/18 +2
 writer.writerow(["Phi [deg]","SUM","DIFF","DIFF-SUM"])
 
 # motor setup
@@ -23,8 +24,9 @@ GPIO.setup(11,GPIO.OUT)  # Set pin 11 as an output, and define as servo1 as PWM 
 servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
 servo1.start(0)
 
+
 base_number = int(input('which base is this: '))
-name = 'Wi-Fi'#str(input('which Wi-Fi AP will you measure?'))
+name = 'allen'#str(input('which Wi-Fi AP will you measure?'))
 freq = gura.get_Channel('wlan0', name)
 print('target wifi SSID: ', name)
 print('target wifi freq: ', freq)
@@ -33,11 +35,13 @@ interface2 = gura.get_interface(base_number)['sum']       # the sum one
 #interface3 = 'wlan1' # the default one
 print('interfaces', interface1, interface2)
 
+servo1.ChangeDutyCycle(duty)
+
 while angle <= 180:
     l_signal1 = []
     l_signal2 = []
     l_r = []
-    for i in range(10):
+    while min(len(l_signal1), len(l_signal2))<10:
         try:
             data1 = gura.get_Signal(interface1, freq, name)
             signal1 = float(data1['signal'])
@@ -61,11 +65,13 @@ while angle <= 180:
     diff_signal = sum(l_signal1) / len(l_signal1)
     sum_signal = sum(l_signal2) / len(l_signal2)
     avg_r = sum(l_r) / len(l_r)
-    writer.writerow( [angle, diff_signal, sum_signal ,diff_signal - sum_signal] )
+    writer.writerow( [int(record_angle), diff_signal, sum_signal ,diff_signal - sum_signal] )
     #print('duty : ', duty)
     angle += each_portion
+    record_angle += each_portion
     duty = angle/18 +2
     servo1.ChangeDutyCycle(duty)
+    print('TURNING')
     time.sleep(1)
 
 print("Finish duty cycle")
--- altered/plot_ground_truth.py
+++ altered/plot_ground_truth.py
@@ -3,12 +3,14 @@ from scipy.interpolate import interp1d
 import pandas as pd
 import numpy as np
 
-data2 = pd.read_csv('../ground_truth.csv')
-x = np.linspace(0, 50, 51)
+data2 = pd.read_csv('/Users/chenfayu/Documents/@@台灣大學電機系＿三上專題研究/演算法/DoA-Locating/altered/ground_truth_new.csv')
+x = np.linspace(0, 90, 91)
+print(x)
 y = data2.iloc[x, 3]
+print(y)
 f = interp1d(x, y, kind = 'linear', fill_value='extrapolate')  # radial basis function interpolator instance
 
-xnew = np.arange(0, 60, 0.5)
+xnew = np.arange(0, 90, 2)
 ynew = f(xnew)   # use interpolation function returned by `interp1d`
 plt.plot(x, y, 'o', xnew, ynew, '-')
 plt.show()
