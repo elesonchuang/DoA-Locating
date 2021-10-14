f4c637f9d7b33e8a35d8b1be6c08985e4fa06234
fafachen61 <71396110+fafachen61@users.noreply.github.com>
2021-10-14 20:36:33 +0800

    plot ground truth

--- altered/plotcode/plot_ground_truth.py
+++ altered/plotcode/plot_ground_truth.py
@@ -3,14 +3,22 @@ from scipy.interpolate import interp1d
 import pandas as pd
 import numpy as np
 
-data2 = pd.read_csv('/Users/chenfayu/Documents/@@台灣大學電機系＿三上專題研究/演算法/DoA-Locating/altered/ground_truth_new0820-2.csv')
-x = np.linspace(0, 90, 91)
-print(x)
-y = data2.iloc[x, 1]
-print(y)
-f = interp1d(x, y, kind = 'linear', fill_value='extrapolate')  # radial basis function interpolator instance
+data = pd.read_csv('/Users/chenfayu/Documents/@@台灣大學電機系＿三上專題研究/演算法/DoA-Locating/altered/ground_truths/ground_truth_new1008-6_2.csv')
+############### parameters
+data_num = len(data) #number of data
+angle_portion = 180/(data_num-1) #angle of each turn
+###############
 
-xnew = np.arange(0, 90, 2)
-ynew = f(xnew)   # use interpolation function returned by `interp1d`
-plt.plot(x, y, 'o', xnew, ynew, '-')
+index = np.linspace(0, data_num-1, data_num)  #to read the csv file
+y = data.iloc[index, 3]  #get the DIFF-SUM 
+
+arr = np.array([])
+for i in range(len(y)):
+    arr = np.append(arr, y[i]) #store DIFF-SUM into array
+x_axis = np.arange(0, 181, angle_portion)   #x-axis
+
+f = interp1d(x_axis, arr, kind = 'linear', fill_value='extrapolate')  # radial basis function interpolator instance
+value = f(x_axis)   # use interpolation function returned by `interp1d`
+
+plt.plot(x_axis, value, 'o', x_axis, value, '-')
 plt.show()
